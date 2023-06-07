b68a508cecae61cc452e9b0820a5c83e92b81dc0
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-07 11:17:18 +0000

    Wed, Jun 7, 2023, 11:17 AM +00:00

--- hello.c
+++ hello.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main (void){
-printf("hello world");
-printf("hello world");
-printf("hello world");
-printf("hello world");
+printf("hello world\n");
+printf("hello world\n");
+printf("hello world\n\n");
+printf("hello world\n");
+string answer=get_string("what is your name?");
+
 
 }
