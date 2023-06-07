23b869e5a00cba129fbe8111abf8cb9f17e2a11d
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-07 11:27:12 +0000

    Wed, Jun 7, 2023, 11:27 AM +00:00

--- hello.c
+++ hello.c
@@ -5,6 +5,8 @@ printf("hello world\n");
 printf("hello world\n");
 printf("hello world\n\n");
 printf("hello world\n");
+
+
 string answer=get_string("what is your name?\n");
 printf("Hello, %s\n",answer);
 
