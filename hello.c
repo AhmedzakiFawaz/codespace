abb3c456ed84a259dd6e02da667ce8b86848a050
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-07 14:10:42 +0000

    Wed, Jun 7, 2023, 2:10 PM +00:00

--- hello.c
+++ hello.c
@@ -8,10 +8,23 @@ int main (void){
 //printf("Hello, %s\n", answer);
 //string Ahmed=get_string("what is your age?");
 //printf ("oh, %s years\n ",Ahmed);
+  //long num1 =get_long("Num1 :");
+  //long num2 =get_long("Num2 :");
+
+  //printf("%li\n",num1+num2);
+
   long num1 =get_long("Num1 :");
   long num2 =get_long("Num2 :");
+  if(num1>=num2){
+   printf("the answer false!");
 
-  printf("%li\n",num1+num2);
+
+
+  }
+  else{
+
+    ("the answer right!");
+  }
 
 
   }
