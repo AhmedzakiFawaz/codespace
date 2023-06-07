1f2257f895049c9da6965d23fe4447ccd95e2131
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-07 12:20:38 +0000

    Wed, Jun 7, 2023, 12:20 PM +00:00

--- hello.c
+++ hello.c
@@ -10,7 +10,3 @@ printf("hello world\n");
 string answer=get_string("what is your name?\n");
 printf("Hello, %s\n", answer);
 string Ahmed=get_string("what is your age?");
-printf ("oh, %s years\n ",Ahmed);
-
-long Ahmed=get_long("what is your age?");
-printf ("oh, %l years\n ",Ahmed);
