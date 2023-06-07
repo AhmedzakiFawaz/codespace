810c5a460dd5b1e39c5925496f3ac44522ec7cd1
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-07 14:34:27 +0000

    Wed, Jun 7, 2023, 2:34 PM +00:00

---
+++ If conditions.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<cs50.h>
+int main (void){
+    char c=get_char('Do you agree?')
+    if (c=='Y'||c=='y'){
+        printf("you are agree");
+
+    }
+     else if(C='n'||'N'){}
+
+}
