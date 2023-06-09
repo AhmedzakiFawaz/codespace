ef2ef139ef50fc089754977d509de1bc329d2214
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-09 15:35:38 +0000

    Fri, Jun 9, 2023, 3:35 PM +00:00

--- string.c
+++ string.c
@@ -9,8 +9,19 @@ int main(void){
     printf("oh, hello %s\n",answer);
 
     int x=55;
-    printf("oh, %i false",x);
+    printf("oh, %i false\n",x);
+
     int num1=get_int("enter num 1; ");
     int num2=get_int("enter num 2; ");
-    printf("%d \n",num1+num2);
+
+   if(num1==num2){
+    printf("num1 equal num2");
+
+   }
+   if_else(num1>num2){
+    printf("num1 greater than num2");
+   }
+   else{
+    printf("num1 less than num2");
+    }
 }
