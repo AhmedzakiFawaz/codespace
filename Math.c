f9b896bda61e2e27b7ddadbd7c84f22a14f29b64
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-09 14:25:30 +0000

    Fri, Jun 9, 2023, 2:25 PM +00:00

---
+++ Math.c
@@ -0,0 +1,9 @@
+    #include<stdio.h>//essential libreary
+    #include<cs50.h>//essential libreary for some commands
+    int main (void){
+
+    int num1=55;
+    int num2=25;
+    printf("num1+num2\n");
+
+    }
