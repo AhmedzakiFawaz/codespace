1ea8afbabbc7a78fe3f2ae3be0bd636ad3730557
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-09 14:20:49 +0000

    Fri, Jun 9, 2023, 2:20 PM +00:00

--- hello.c
+++ hello.c
@@ -1,44 +1,7 @@
-#include<stdio.h>
-#include<cs50.h>
+#include<stdio.h>//essential libreary
+#include<cs50.h>//essential libreary for some commands
 int main (void){
 
-
-
-//string answer=get_string("what is your name?\n");
-//printf("Hello, %s\n", answer);
-//string Ahmed=get_string("what is your age?");
-//printf ("oh, %s years\n ",Ahmed);
-  //long num1 =get_long("Num1 :");
-  //long num2 =get_long("Num2 :");
-
-  //printf("%li\n",num1+num2);
-
- int num1 =get_int("Num1 :");
- int num2 =get_int("Num2 :");
-
-  if(num1>num2){
-   printf("the answer false!");
-
-  }
-
-   else if(num1==num2){
-
-    printf("num1=num2");
-   }
-
- else{
-
-    printf("the answer right!");
-  }
-
-  string jak=get_string("what is your name?");
-  printf("oh, hello %s",jak);
-
-
-in
-  while(1<3){
-    printf("meow\n");
-
-  }
+printf
 
   }
---
+++ string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<cs50.h>
+
+//you should to use %f with float or double and //%c with char //ld or li with long
+
+int main(void){
+
+    string answer=get_string("what is your name?\n");
+    printf("oh, hello %s\n",answer);
+
+    int x=55;
+    printf("oh, %i false",x)
+}
