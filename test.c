6491ea2090bd15b75fbe865540cd94fb6f4326be
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-10 23:49:02 +0000

    Sat, Jun 10, 2023, 11:49 PM +00:00

--- test.c
+++ test.c
@@ -5,12 +5,18 @@
 
    for(int i=0; i<10; i++){
 
-    printf("%i\n");
+    printf("mewo\n");
    }
-
   }
 
-int main() {
+
+    void type2(int num1){
+   int result=num1*10;
+   printf("enter your number: ");
+
+    }
+    int main() {
 
 type1();
+type2(25);
 }
