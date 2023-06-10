587384f3bad08fd451feed8b1a736d4709d62a0e
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-10 01:20:26 +0000

    Sat, Jun 10, 2023, 1:20 AM +00:00

--- functions.c
+++ functions.c
@@ -6,18 +6,28 @@
    void hello(void)
 
    {
+      int i;
       for(i=0; i<25; i++){
          printf("%i \n",i);
       }
 
    }
 
+   //variable functions
+   int variable(int n)
+   {
+   int x;
+   for(x=0; x<n; x++){
+   printf("%i \n",x);
+   }
+   }
 
 
    int main (void)
 
    {
       hello();
+      variable(25);
 
 
 
