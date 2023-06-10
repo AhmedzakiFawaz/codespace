76a7ef881644c21d701dd7b17df8ffa15e9b7f12
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-10 22:46:37 +0000

    Sat, Jun 10, 2023, 10:46 PM +00:00

---
+++ test.c
@@ -0,0 +1,34 @@
+ #include<stdio.h>
+    #include<cs50.h>
+    // Void function
+void print_hello_world() {
+  printf("Hello, world!\n");
+}
+
+// Parameter function
+int add(int a, int b) {
+  return a + b;
+}
+
+// Return function
+int factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+int main() {
+  // Call void function
+  print_hello_world();
+
+  // Call parameter function and assign the returned value to a variable
+  int sum = add(10, 20);
+
+  // Call return function and print the returned value
+  int factorial_of_5 = factorial(5);
+  printf("The factorial of 5 is %d\n", factorial_of_5);
+
+  return 0;
+}
