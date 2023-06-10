8f764534468638b9d0057f8117d1b29405faa7a8
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-10 01:15:24 +0000

    Sat, Jun 10, 2023, 1:15 AM +00:00

--- functions.c
+++ functions.c
@@ -1,47 +1,24 @@
 #include<stdio.h>
 #include<cs50.h>
 
+//void functions
 
-void first (void){
-
-   for(int z=0; z<25; z++){
-   printf("%i \n",z);}
-}
-
-
-
-void hello(int n )//argument
-{
-
-   for(int i=0; i<n ; i++)
+   void hello(void)
 
    {
-   printf("gg\n");
+      for(i=0; i<25; i++){
+         printf("%i \n",i);
+      }
+
    }
-}
 
 
 
-int sum(int n1, int n2){
+   int main (void)
 
- printf("%i\n",n1+n2);
-   return n1+n2;
-}
-
-
-int main (void)
-
-{
-first();
-hello();
+   {
+      hello();
 
 
 
-int x= sum(25, 55);
-int y=55;
-
-printf("%i\n",x+y);
-
-
-
-}
+   }
