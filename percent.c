b65a5dec31739ea44b4a5e2d28461990e6b7ddab
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 11:39:37 +0000

    Sun, Jun 11, 2023, 11:39 AM +00:00

--- percent.c
+++ percent.c
@@ -2,22 +2,12 @@
     #include<cs50.h>
 
 
-    int fun(int num1, int disc ){
-
-    int percent=num1*.85;
-    return percent;
-    int disc=num1-(100-disc)/100;
-
- }
 
     int main() {
 
-
-     float  num1=get_int("enter your price: ");
-     int disc=get_int("enter your discount: ")
-     int sale=fun(num1, disc);
-     printf("%i \n",sale);
-      printf("%i \n",disc);
+ float price=get_float("enter your price: \n");
+  int disc=price*.84;
+ printf("%.2f \n",disc);
 
 
 }
