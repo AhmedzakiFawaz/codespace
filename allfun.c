e5a28bf4bceaf9a7480c0f45447c390ded5e7e6a
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 15:05:29 +0000

    Sun, Jun 11, 2023, 3:05 PM +00:00

--- allfun.c
+++ allfun.c
@@ -6,10 +6,31 @@
         printf("Meow\n");
     }
     //paramater function
-    void type
+    void type2(int num1, int num2){
+
+      int sum=num1*num2;
+       printf("%i \n",sum);
+
+      int type3(float x){
+        int price=x*.85;
+
+
+
+       }
+
+
+    }
+
+
+
     int main(){
         type1();
          type1();
+         type2(5, 10);
+
+         float price=get_float("enter your estimaited price: ");
+         float sum=type3(price)
+         printf("%.2f \n",sum);
 
 
     }
