d285645469d5d4076d75a1de5efc04c1f31c47f9
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 11:51:17 +0000

    Sun, Jun 11, 2023, 11:51 AM +00:00

--- percent.c
+++ percent.c
@@ -1,7 +1,7 @@
  #include<stdio.h>
     #include<cs50.h>
 
-   int discount(int n, int ){
+   int discount(int n, int Z){
 
    int disc=n*.85;
    return disc;
@@ -11,6 +11,7 @@
     int main() {
 
         float price=get_float("enter your price: \n");
+        int   discount=get_int
         int disc=discount(price);
         printf("Your discounted price = %i \n",disc);
 
