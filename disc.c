40032e789b33ae090cb3e157f62869dd50673dcb
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 14:28:00 +0000

    Sun, Jun 11, 2023, 2:28 PM +00:00

--- disc.c
+++ disc.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<cs50.h>
-float
+float disc(float pric , int disc){
+
+    float main=pric*(100-disc)/100;
+    return main;
+}
 
 int main(){
     float price=get_float("enter your price: ");
     int discount=get_int("enter your discount: ");
-    float sale=disc(price,discount)
+    float sale=disc(price,discount);
+    printf("%.2f \n",sale);
 
 
 }
