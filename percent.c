ef40bdd5bd0013666eb55b1f959c02fcb16518ff
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 12:58:36 +0000

    Sun, Jun 11, 2023, 12:58 PM +00:00

--- percent.c
+++ percent.c
@@ -3,16 +3,16 @@
 
    int discount(int n int z){
 
-    int sale=price*(100-discount)/100;
+    int sale=price*(100-precent)/100;
 
    }
 
 
     int main() {
 
-        float price=get_float("enter your price: \n");
+        float regular=get_float("enter your price: \n");
 
-        int disc=get_int("please enter discount: ");
+        int percent=get_int("please enter discount: ");
 
       float sale=discount(n,z);
       printf("sale price : %f \n",sale)
