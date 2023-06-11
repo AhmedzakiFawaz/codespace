56b3ad26c576200caad33ea3652c3079ee857eed
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 08:50:04 +0000

    Sun, Jun 11, 2023, 8:50 AM +00:00

--- test.c
+++ test.c
@@ -1,31 +1,16 @@
  #include<stdio.h>
     #include<cs50.h>
-    // Void function
-  void type1(){
 
-   for(int i=0; i<10; i++){
+    int discount(int sale){
 
-    printf("mewo\n");
-   }
-  }
-
-
-    void type2(int num1){
-   int result=num1*10;
-     printf("%i\n",result);
-
-    }
-    int type3(int num1, int num2){
-      int sum=num1*num2;
-      printf("%i\n",sum);
-      return sum;
+      int price=sale*.85;
+      return price;
     }
 
     int main() {
 
-type1();
-type2(25);
-type3(25, 2);
-int result=type3(25,15);
-printf("%i\n",result);
+
+int regular=get_int("enter your regular price: ");
+int sale=discount(regular);
+printf("%i \n",sale);
 }
