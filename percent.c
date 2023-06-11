41aab3ed0571162024a94490442d63eacb9a007f
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 11:10:46 +0000

    Sun, Jun 11, 2023, 11:10 AM +00:00

---
+++ percent.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+    #include<cs50.h>
+
+        float percent (float price int percent){
+
+
+        }
+
+    int main() {
+
+
+     int  num1=get_int("enter your price: ");
+     int  num2=get_int("enter your discount: ");
+
+
+
+
+}
