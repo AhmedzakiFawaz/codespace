6bae9de1fc9182ff964270b0ccda01536fbaa5d7
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 11:18:00 +0000

    Sun, Jun 11, 2023, 11:18 AM +00:00

--- percent.c
+++ percent.c
@@ -1,18 +1,20 @@
  #include<stdio.h>
     #include<cs50.h>
 
-        float percent (float num1 int num2){
 
-                
+    int sale(int num1 ){
 
-        }
+
+
+
+ }
 
     int main() {
 
 
-     int  num1=get_int("enter your price: ");
-     int  num2=get_int("enter your discount: ");
-
+     float  num1=get_int("enter your price: ");
+     int sale=sale(num1);
+     printf("%i \n",sale);
 
 
 
