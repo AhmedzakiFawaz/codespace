08d3a9faa0188e983afe24e8f09732e5a5b68a5c
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 14:23:38 +0000

    Sun, Jun 11, 2023, 2:23 PM +00:00

---
+++ disc.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<cs50.h>
+float
+
+int main(){
+    float price=get_float("enter your price: ");
+    int discount=get_int("enter your discount: ");
+    float sale=disc(price,discount)
+
+
+}
