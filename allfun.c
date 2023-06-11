c3c644a62f3ea32bfe412cc7f228d2cb352e9478
AhmedzakiFawaz <133858562+AhmedzakiFawaz@users.noreply.github.com>
2023-06-11 15:17:57 +0000

    Sun, Jun 11, 2023, 3:17 PM +00:00

--- allfun.c
+++ allfun.c
@@ -14,7 +14,7 @@
 
 
     }
-
+//return function
         int type3(float x){
         int price=x*.85;
         return price;
@@ -22,7 +22,15 @@
 
 
        }
+        int type4(float price, int disc){
 
+                float final=price*(100-disc)/100;
+                return final;
+
+
+
+
+        }
 
     int main(){
         type1();
@@ -30,8 +38,13 @@
          type2(5, 10);
 
          float price=get_float("enter your estimaited price: ");
-         float sum=type3(price)
-         printf("%.2f \n",sum);
+         float sum=type3(price);
+         printf("price after discount is= %.2f \n",sum);
+
+         float price2=get_float("enter your second price : \n");
+         int disc=get_int("enter your discount: \n");
+        int final=type4(price,disc);
+        printf("total price after discount is:%i ",final);
 
 
     }
